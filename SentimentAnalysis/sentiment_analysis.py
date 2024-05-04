942419ca31f2601a921c495f1c6228327de4595a
psokhi <preetpal.sokhi@gmail.com>
2024-05-04 16:01:43 -0400

    Update sentiment_analysis.py

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -6,8 +6,11 @@ def sentiment_analyzer(text_to_analyze):
     headers =  {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
     myobj =  { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = myobj, headers=headers)
-    formatted_response = json.loads(response.text)
-    label = formatted_response['documentSentiment']['label']
-    score = formatted_response['documentSentiment']['score']
-
+    if reponse.status_code != 400:
+        formatted_response = json.loads(response.text)
+        label = formatted_response['documentSentiment']['label']
+        score = formatted_response['documentSentiment']['score']
+    else: 
+        label = None
+        score = None
     return {'label':label, 'score':score}
